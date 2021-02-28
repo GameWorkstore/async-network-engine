741b20d5d226625026cb3e7acda436d92e3f9aa8
Hanza Ru <woodimbr@gmail.com>
2021-02-27 22:42:12 -0300

    +More progress

--- Assets/Tests/GCP/main.go
+++ Assets/Tests/GCP/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
-	//"github.com/GameWorkstore/async-network-engine/Assets/asyncnetworkengine"
+
+	ase "github.com/GameWorkstore/async-network-engine/Assets/asyncnetworkengine"
 )
 
 // Process handler of this test
 func Process(w http.ResponseWriter, r *http.Request) {
-	//var resp GenericMessegeResponse
-	//GCPDecode(r,w,GenericMessegeResponse)
+	//var resp ase.GenericMessegeResponse
+	ase.GCPDecode(r, w, nil)
 }
